e13c7588dde462b60a6c968f11080905b1a4bb4a
지수환 <su010604@korea.ac.kr>
2024-07-22 11:29:29 +0900

    API_URL from page -> constants

--- components/movie-credits.jsx
+++ components/movie-credits.jsx
@@ -1,4 +1,4 @@
-import {API_URL} from "../app/(home)/page";
+import {API_URL} from "../app/constants";
 import styles from "../styles/movie-credits.module.css";
 import {getMovie} from "../components/movie-info";
 
